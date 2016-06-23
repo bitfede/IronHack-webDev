0c147755959d67bd2e77746d4a541a6c75255b0f
Fede Defaveri <f.g.defaveri@gmail.com>
2016-06-23 11:28:31 -0400

    learned about automated testing and started the blog sinatra app

--- week2/day4/Lecture_example/spec/app_spec.rb
+++ week2/day4/Lecture_example/spec/app_spec.rb
@@ -4,9 +4,32 @@ require 'sinatra'
 require 'rack/test'
 
 describe 'Server Service' do
-	include RAck::Test::Methods
+	include Rack::Test::Methods
 
 	def app
-				Sinatra::Application
-	end	
+		Sinatra::Application
+	end
+
+	it "Should load the home page" do
+		get '/'
+		expect(last_response).to be_ok
+	end
+
+	it "Should not load the home page" do
+		get '/home'
+		expect(last_response).to_not be_ok
+	end
+
+	it "Should load the other page" do
+		get '/real_page'
+		expect(last_response).to be_ok
+	end
+
+	it "should redirect to the real_page" do 
+		get '/hi'
+		expect(last_response.redirect?).to be(true)
+		follow_redirect!
+		expect(last_request.path).to eq('/real_page')
+	end
+
 end
---
+++ week2/day4/Sinatra_Blog/app.rb
@@ -0,0 +1,8 @@
+require_relative("models/Blog.rb")
+require_relative("models/Post.rb")
+
+require 'sinatra'
+
+get '/' do
+	"HELLO!"
+	end
