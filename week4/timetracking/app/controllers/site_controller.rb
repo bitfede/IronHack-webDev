5a6a3f0fa6d0c278a05b1077276dd53f9ff565b4
Fede Defaveri <f.g.defaveri@gmail.com>
2016-07-04 11:09:33 -0400

    added route controller and view for  homepage and contacts page

--- week4/timetracking/app/controllers/site_controller.rb
+++ week4/timetracking/app/controllers/site_controller.rb
@@ -1,2 +1,15 @@
 class SiteController < ApplicationController
+
+	def home
+
+		render 'home' #name of the view template
+
+	end
+	
+	def contacts
+
+		render 'contacts' #name of the contacts template
+		
+	end
+
 end
