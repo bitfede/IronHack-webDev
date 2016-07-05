34913aea96816bc877bc97ea8d57af1228cfa9f5
Fede Defaveri <f.g.defaveri@gmail.com>
2016-07-05 12:30:06 -0400

    did a db migration and played around with the database queries

--- week4/timetracking/db/schema.rb
+++ week4/timetracking/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705141723) do
+ActiveRecord::Schema.define(version: 20160705145112) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
