9b6a25b2e898b98051490a4d41c0a689957ba854
Fede Defaveri <f.g.defaveri@gmail.com>
2016-07-06 16:04:29 -0400

    added the time_entry controller and views -- user can now add time entries for each project

--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
   get '/contacts', to: 'site#contacts'
   get '/projects', to: 'projects#index'
   get '/projects/new', to: 'projects#new'
-	get '/projects/show/:id', to: 'projects#show'
+	get '/projects/:id', to: 'projects#show'
 
 	post '/projects', to: 'projects#create'
+	
+	get '/projects/:project_id/time_entries', to: 'time_entries#index'
+	get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
+
+	post '/projects/:project_id/time_entries', to: 'time_entries#create', as: :project_time_entries
 	
 end
