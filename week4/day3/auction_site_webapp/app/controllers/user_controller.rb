1389967a52e8a9eccc7bc0c5d60c693db496d76c
Fede Defaveri <f.g.defaveri@gmail.com>
2016-07-06 20:09:48 -0400

    created the new user form, which succesfully saves the new data to DB

--- week4/day3/auction_site_webapp/app/controllers/user_controller.rb
+++ week4/day3/auction_site_webapp/app/controllers/user_controller.rb
@@ -1,2 +1,25 @@
 class UserController < ApplicationController
+	def new
+
+		@new_user = User.new
+		render 'new'
+
+	end
+
+	def create
+
+		@new_user = User.new(
+			:name => params[:user][:name],
+			:email => params[:user][:email]
+			)
+
+		if (@new_user.save)
+
+			redirect_to('/')
+
+		else
+			render 'new'
+		end
+
+	end
 end
--- week4/day3/auction_site_webapp/config/routes.rb
+++ week4/day3/auction_site_webapp/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'site#home'
+  get '/users/new', to: 'user#new'
+  post '/users', to: "user#create"
 end
