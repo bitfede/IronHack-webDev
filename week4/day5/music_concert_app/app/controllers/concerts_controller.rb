560db78429a4578960c21324ce131c331247d41e
Fede Defaveri <f.g.defaveri@gmail.com>
2016-07-08 16:03:37 -0400

    added the new concert functionality, now the user can submit new concert data to the database

--- week4/day5/music_concert_app/app/controllers/concerts_controller.rb
+++ week4/day5/music_concert_app/app/controllers/concerts_controller.rb
@@ -2,4 +2,26 @@ class ConcertsController < ApplicationController
 	def home
 		render 'home'
 	end
+
+	def new
+		@new_concert = Concert.new
+		render 'new'
+	end
+
+	def create
+		@new_concert = Concert.new(
+			:artist 				=> params[:concert][:artist],
+			:venue 					=> params[:concert][:venue],
+			:city 					=> params[:concert][:city],
+			:date 					=> params[:concert][:date],
+			:price 					=> params[:concert][:price],
+			:description 		=> params[:concert][:description]
+			)
+
+		if @new_concert.save
+			redirect_to '/'
+		else
+			render 'new'
+		end
+	end
 end
--- week4/day5/music_concert_app/config/routes.rb
+++ week4/day5/music_concert_app/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'concerts#home'
-  
-  
+
+  resources :concerts, only: [:show, :new, :create, :index]
+
 end
