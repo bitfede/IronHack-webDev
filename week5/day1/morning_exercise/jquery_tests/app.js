f3cccf98224682b5ef6ed6c5e917b90cbb39f551
Fede Defaveri <f.g.defaveri@gmail.com>
2016-07-12 18:12:52 -0400

    modified auction app, added week5 day1&2 exercises

---
+++ week5/day1/morning_exercise/jquery_tests/app.js
@@ -0,0 +1,21 @@
+
+
+$(document).on('ready', function() {
+
+	$('.fadeButton').on('click', function() {
+
+		// alert('pre')
+		$('.fadee').fadeToggle(1000);
+		// alert('post')
+	} )
+
+
+	// for (var i = 0; i < 100; i++ ) {
+	// 	$('.olympics').fadeToggle(1000);
+	// 	$('.olympics').fadeToggle(1000);
+
+	// }
+
+	
+
+})
