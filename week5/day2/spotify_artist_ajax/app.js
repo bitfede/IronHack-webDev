e1aedb19968c43bb3d783e3e40466788b11de6f6
Fede Defaveri <f.g.defaveri@gmail.com>
2016-07-13 18:33:44 -0400

    created an interactive spotify artist search

--- week5/day2/spotify_artist_ajax/app.js
+++ week5/day2/spotify_artist_ajax/app.js
@@ -1,21 +1,85 @@
 $(document).ready( function() {
-
+	 $(document).foundation();
 	 $('.js-submit').fadeToggle(1);
 	 $('.js-submit').fadeToggle(1000);
 
-	 function clickbutton() {
-	 var artist = $('.js-artist-src').val()
+	 function getArtist(event) {
+	 $('.artists').empty()
+	 event.preventDefault();
+	 var artist = $('.js-artist-src').val();
+	 sendArtReq(artist);
 
+		}
 
+	function sendArtReq(artist) {
+		var urlArt = `https://api.spotify.com/v1/search?type=artist&query=${artist}`;
+		
 	 $.ajax({
-	 	url: "https://api.spotify.com/v1/search?type=artist&query=" + artist,
-	 	error: function() {alert('ERROR')},
-	 	success: function() {alert('GOOD')}
+	 	url: urlArt,
+	 	error: error,
+	 	success: successArtist
 	 });
 
 	}
 
+	function error() {
+		console.log("ERROR");
+		alert('error');
+	}
 
-	$('#js-submit').on('click', clickbutton);
+	function successArtist(obj) {
+		var artists = obj.artists.items;
+		printToPage(artists);
+	}
+
+	function sendAlbumReq(artistID) {
+
+	}
+
+	function printToPage(artists) {
+
+		for (var i = 0; i < artists.length; i++) {
+		art = artists[i];
+		console.log(art);
+		var imgs = makeGrid(art);
+
+		modal = `
+		<div class="reveal" id="Modal${art.id}" data-reveal>
+	  <h1>${art.name}</h1>
+	  <p class="lead">This artist ID is ${art.id}</p>
+	  <p>Im a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+	  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+	  </button>
+		</div>
+		`;
+		$('.artists').prepend(modal);
+		html = `
+			
+			<hr>
+			<h3><a data-open="Modal${art.id}">${art.name}</a></h3>
+			${imgs}
+		`;
+
+		$('.artists').append(html)
+		$(document).foundation();
+		}
+	}
+
+	function makeGrid(artist) {
+	if (artist.images.length === 0) {
+			return "[no pictures]";
+		}
+		else {
+			html = `<div class="row small-up-1 medium-up-2 large-up-4">`
+			html = `${html}<div class="column"><img src="${artist.images[0].url}" class="thumbnail" alt="">
+			</div>
+			`;
+			html = `${html}</div>`;
+			return html
+		}
+	}
+
+	$('#js-submit').on('click', getArtist);
 
 });
