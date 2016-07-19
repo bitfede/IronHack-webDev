495a0c41430e6267e470c346f07badf80294aa88
Fede Defaveri <f.g.defaveri@gmail.com>
2016-07-19 16:57:21 -0400

    added all today's exercises

---
+++ week6/day2/auth-exercise/app/controllers/application_controller.rb
@@ -0,0 +1,29 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+  	if @current_user.nil?
+  		@current_user = User.find_by(id: session[:user_id])
+  	end
+  	@current_user
+  end
+
+  def authorize_user
+  	unless current_user
+  	flash[:message] = "Please log in or register to access this page"
+  	redirect_to "/"
+  	end 
+	end 
+
+	def admin_only
+		if current_user.nil? || current_user.role != "admin"
+			flash[:access_denied] = "Acess denied. You must be an admin to see this"
+
+			redirect_to "/"
+		end
+	end
+
+end
